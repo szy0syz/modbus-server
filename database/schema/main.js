2fbc9b8e505b4ae184745ac869fa7ff81d12a7bc
szy0syz <szy0syz@gmail.com>
2018-06-15 20:20:34 +0800

    add mongoose

---
+++ database/schema/main.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const MainSchema = new Schema({
+  _optCode: Schema.Types.Buffer,
+  _count: Schema.Types.Number,
+  _oriData: Schema.Types.Buffer,
+  _data: [
+    {
+        date: Schema.Types.Date
+    }
+  ],
+  meta: {
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now()
+    }
+  }
+})
+
+// 虚拟字段，没有实际存入数据库中，类似计算字段
+UserSchema.virtual('isLocked').get(function () {
+  return true
+})
+
+
+
+mongoose.model('main', UserSchema)
